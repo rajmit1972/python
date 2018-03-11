95b022eb8b4d8a6978dd2327d7cc37765368a9a2
rajmit1972 <rajmit1972@gmail.com>
2018-03-11 19:41:01 +0000

    asdfasdf

---
+++ test1.py
@@ -0,0 +1,11 @@
+ Volume in drive C has no label.
+ Volume Serial Number is 68C3-8081
+
+ Directory of C:\Users\Administrator\Desktop\python
+
+03/11/2018  07:40 PM    <DIR>          .
+03/11/2018  07:40 PM    <DIR>          ..
+03/11/2018  07:40 PM                 0 test1.py
+03/11/2018  07:14 PM                24 test2.py
+               2 File(s)             24 bytes
+               2 Dir(s)  14,596,300,800 bytes free
